712e2788a687f577b4597ccb5bdbf9307ace99ad
Petter Taule <pettertaule@live.no>
2017-01-24 16:47:32 +0100

    Added timing of C++ program

--- Preassignment/src/randomWalk.cpp
+++ Preassignment/src/randomWalk.cpp
@@ -8,19 +8,25 @@
 #include<iostream>
 #include<random>
 #include<armadillo>
+#include<ctime>
+#include<thread>
 
 using namespace arma;
 
 const int N = 1000;
+const int threads = 4;
 
 Col<int> distribution = zeros<Col<int>>(N);
 
 int main(int argc, char *argv[]) {
+    clock_t time;
+    time = clock();
+
     std::random_device seeder;
     std::default_random_engine generator(seeder());
     std::uniform_int_distribution<int> uniform(0,1);
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N/4; ++i) {
         int sum = 0;
         int steps = 0;
 
@@ -37,5 +43,8 @@ int main(int argc, char *argv[]) {
     }
 
     distribution.save("distribution.txt", raw_ascii);
+
+    time = clock() - time;
+    std::cout << "Programmet brukte " << static_cast<double>(time)/CLOCKS_PER_SEC << " sekunder" << std::endl;
     return 0;
 }
