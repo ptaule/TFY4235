a27369f955403e383631f0adb6c360aa7bd80343
Petter Taule <pettertaule@live.no>
2017-02-06 16:56:37 +0100

    New tests and updated Makefile for tests

--- Assignment 1/Task 2/tests/tests.cpp
+++ Assignment 1/Task 2/tests/tests.cpp
@@ -5,12 +5,36 @@
    Copyright (c) 2017 Petter Taule. All rights reserved.
 */
 
+#include<iostream>
 #include"../src/diffusion.hpp"
 
-real cflConstant() {
+using namespace arma;
+
+const realNum epsilon = 0.001;
+
+void cflConstant() {
+    const realNum alpha = getALPHA();
+    if (alpha > 0.5) {
+        std::cout << "Warning! Alpha = " << alpha << " > 0.5." << std::endl;
+    }
+    else {
+        std::cout << "Alpha = " << alpha << std::endl;
+    }
 
 }
 
+
+void sumConcentration() {
+    Col<realNum> U = zeros<Col<realNum>>(INTERVALS);
+    U(INTERVALS/2) = 1;
+    eulerExplicit(U, INTERVALS, N);
+    std::cout << "Summen av U er " << sum(U) << std::endl;
+    std::cout << "U_0 = " << U_0 << std::endl;
+}
+
+
 int main(int argc, char *argv[]) {
+    cflConstant();
+    sumConcentration();
     return 0;
 }
